e8ab6fa50ac380b7d48858ceee097af746bf849f
JulianoAureliano <juliano.aureliano2013@gmail.com>
2025-12-02 17:36:35 -0300

    Comecei o historico

--- Historico.cpp
+++ Historico.cpp
@@ -4,28 +4,52 @@
 #include <iomanip>
 #include <sstream>
 
+using namespace std;
 // Adicionar registro ao histórico
-void Historico::adicionarRegistro(const RegistroTreino& registro) {
+void Historico::adicionarRegistro(const RegistroTreino &registro)
+{
     // Implementar
+    registros.push_back(registro);
 }
 
 // Exibir histórico completo
-void Historico::exibirHistorico() const {
+void Historico::exibirHistorico() const
+{
     // Implementar
+    cout << "\n--- HISTÓRICO DE EXECUCOES (" << registros.size() << " total) ---" << endl;
+    if (registros.empty())
+    {
+        cout << "Nenhum registro de execucao no histórico." << endl;
+    }
+    else
+    {
+        for (const auto &registro : registros)
+        {
+            cout << "\n- Data/Hora: " << registro.dataHora << endl;
+            cout << "  - Ficha: " << registro.nomeFicha << " (ID: " << registro.idFicha << ")" << endl;
+            cout << fixed << setprecision(1);
+            cout << "  - Tempo Gasto (Real): " << registro.tempoTotal << " minutos" << endl;
+            cout << "  - Calorias Gastas (Real): " << registro.caloriasTotal << " kcal" << endl;
+        }
+    }
+    cout << "-----------------------------------------------" << endl;
 }
 
 // Getter de registros
-const std::vector<RegistroTreino>& Historico::getRegistros() const {
-    static std::vector<RegistroTreino> vazio; // Placeholder para compilar
-    return vazio; // Implementar
+const vector<RegistroTreino> &Historico::getRegistros() const
+{
+    static vector<RegistroTreino> vazio; // Placeholder para compilar
+    return registros;
 }
 
 // Carregar histórico do arquivo
-void Historico::carregarDeArquivo() {
+void Historico::carregarDeArquivo()
+{
     // Implementar
 }
 
 // Salvar histórico no arquivo
-void Historico::salvarEmArquivo() const {
+void Historico::salvarEmArquivo() const
+{
     // Implementar
 }
